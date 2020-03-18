77a578555fab3c85c4cebaa65f7dafa5e90ccea3
Max <maxbendas86@gmail.com>
2020-03-18 14:43:54 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,11 @@
+const formSearch = document.querySelector('.form-search');
+const inputCitiesFrom = document.querySelector('.input__cities-from');
+const dropdownCitiesFrom = document.querySelector('.dropdown__cities-from');
+const inputCitiesTo = document.querySelector('.input__cities-to');
+const dropdownCitiesTo = document.querySelector('.dropdown__cities-to');
+const inputDateDepart = document.querySelector('.input__date-depart');
+
+const obj = {
+  key: 'value',
+  'key two': 'value2'
+}
